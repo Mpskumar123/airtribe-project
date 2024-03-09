10509d7fb92326fe7068657d6859c5c21f3d1845
Mummana pavan sai kumar <pavansaikumar49@gmail.com>
2024-03-09 17:14:05 +0530

    Intial set up

---
+++ TaskTrack/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="container">
+    <div>
+      <app-logo/>
+      <h1 class="title">
+        tasktrack
+      </h1>
+      <h2 class="subtitle">
+        The  Task Tracker is a web-based application designed for Airtribe interviews to streamline task management and enhance productivity within the Airtribe organization.
+      </h2>
+     
+    </div>
+  </section>
+</template>
+
+<style>
+@import url('../css/index.css');
+</style>
